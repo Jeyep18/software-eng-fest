6da4b9b64b76ea2fac9243ba2c417c0168243ea3
Christ@KuRiSu <cjpotpot91@gmail.com>
2026-03-26 11:31:00 +0800

    Replaced the old monologue ui

--- game/objects/innerMonologue.gd
+++ game/objects/innerMonologue.gd
@@ -1,123 +1,86 @@
 class_name InnerMonologue
 extends Interactable
 
-@export var interaction_prompt: String = "PRESS E NGANI"
-
-## Each entry is one "page" of inner monologue. Supports BBCode tags ([wave], etc.)
+@export var interaction_prompt: String = "PRESS E"
 @export var monologue_lines: Array[String] = [
-	"Hello, I'm a box.",
-	"I'm newly implemented.",
-	"Please handle me well.",
-	"Bye...",
+	"This is a box...",
+	"I wonder what I can do with this.."
 ]
-
-## If true: auto-dismisses after the last line finishes typing.
-## If false: player must press interact once more to close the last line.
 @export var auto_dismiss: bool = false
-
-## Typing speed in characters per second.
 @export var chars_per_second: float = 20.0
+@export var monologue_ui_scene: PackedScene
 
+const PLAYER_NAME: String = "Player"
+
+var _ui: MonologueUI = null
 var _current_line: int = 0
 var _is_showing: bool = false
 var _is_typing: bool = false
 var _tween: Tween = null
 
-
-#region Lifecycle
 func _ready() -> void:
 	super._ready()
 	prompt_label = interaction_prompt
 	player_exited.connect(_on_player_left)
-	_hide_monologue()
-#endregion
+	_setup_ui()
 
+func _setup_ui() -> void:
+	if monologue_ui_scene == null:
+		push_error("InnerMonologue: monologue_ui_scene not assigned on " + name)
+		return
+	_ui = monologue_ui_scene.instantiate() as MonologueUI
+	%CanvasLayer.add_child(_ui)
+	_ui.hide_ui()
 
-#region Interact Override
 func interact() -> void:
 	if not _is_showing:
 		_current_line = 0
 		_show_current_line()
 		return
-	
 	if _is_typing:
 		_skip_to_line_end()
 		return
-	
 	_current_line += 1
 	if _current_line >= monologue_lines.size():
 		_hide_monologue()
 		return
-	
 	_show_current_line()
-#endregion
 
-
-#region Display
 func _show_current_line() -> void:
 	is_showing = true
-	var line: String = monologue_lines[_current_line]
-	
-	%RichTextLabel.bbcode_enabled = true
-	%RichTextLabel.text = line
-	%RichTextLabel.visible_ratio = 0.0
-	
 	_is_showing = true
 	_is_typing = true
-	%BoxContainer.show()
-	
-	_update_line_counter()
+	var line: String = monologue_lines[_current_line]
+	_ui.show_line(line, PLAYER_NAME)
+	_ui.set_prompt_visible(false)
 	prompt_visibility_changed.emit(false)
-	_run_typewriter(line)
-
-
-func _run_typewriter(line: String) -> void:
 	if _tween and _tween.is_valid():
 		_tween.kill()
-	
-	var duration: float = float(line.length()) / chars_per_second
-	_tween = create_tween()
-	_tween.tween_property(%RichTextLabel, "visible_ratio", 1.0, duration)
+	_tween = _ui.run_typewriter(chars_per_second)
 	_tween.finished.connect(_on_typewriter_finished, CONNECT_ONE_SHOT)
-
 
 func _skip_to_line_end() -> void:
 	if _tween and _tween.is_valid():
 		_tween.kill()
-	%RichTextLabel.visible_ratio = 1.0
+	_ui.skip_to_end()
+	_ui.set_prompt_visible(true)
 	_is_typing = false
-
 
 func _hide_monologue() -> void:
 	is_showing = false
 	if _tween and _tween.is_valid():
 		_tween.kill()
-	%BoxContainer.hide()
-	%RichTextLabel.visible_ratio = 0.0
+	_ui.hide_ui()
 	_is_showing = false
 	_is_typing = false
 	_current_line = 0
 	prompt_visibility_changed.emit(true)
 
-
-func _update_line_counter() -> void:
-	if not is_instance_valid(%LineCounter):
-		return
-	if monologue_lines.size() > 1:
-		%LineCounter.text = "%d / %d" % [_current_line + 1, monologue_lines.size()]
-		%LineCounter.show()
-	else:
-		%LineCounter.hide()
-#endregion
-
-
-#region Signal Callbacks
 func _on_typewriter_finished() -> void:
 	_is_typing = false
+	_ui.set_prompt_visible(true)
 	if auto_dismiss and _current_line >= monologue_lines.size() - 1:
 		_hide_monologue()
 
-
 func _on_player_left(_interactable: Interactable) -> void:
 	_hide_monologue()
-#endregion
