b899b54bdb74ac6bf9d785c359afa6c28f481ca3
Enryu <lennardkylebelleza04@gmail.com>
2026-04-18 17:37:20 +0800

    added Pause Menu (working but buttons doesnt have functions yet)

--- game/autoload/SceneManager.gd
+++ game/autoload/SceneManager.gd
@@ -9,7 +9,7 @@ var _pending_spawn_id: String = ""
 
 const SCENE_PATHS: Dictionary = {
 	"act1":          "res://game/scenes/act1/Act1.tscn",
-	"home":          "res://game/scenes/locations/house_1.tscn",
+	"home":          "res://game/scenes/locations/house_inside.tscn",
 	"test_room1":    "res://game/maps/test_map1/room_1.tscn",
 	"test_room2":    "res://game/tests/test_world/test_world.tscn",
 	"tindahan":      "res://game/scenes/locations/tindahan.tscn",
